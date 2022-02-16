0753fc53c0b4e9b6e098487d1c68b2168862701c
Oskar <Oskar.herdenberg@gmail.com>
2022-02-16 16:06:42 +0100

    Sorting out layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import LeftColumn from "./components/LeftColumn";
 import RightColumn from "./components/RightColumn";
 import Main from "./components/Main";
 
+
 function App() {
   return (
-    <BrowserRouter>
+    <div>
       <Header />
-      <div className="container">
-        <Routes>
-          <Main />
-          <RightColumn />
-          <LeftColumn />
-        </Routes>
-      </div>
+      <Main />
+      <RightColumn />
+      <LeftColumn />
       <Footer />
-    </BrowserRouter>
+    </div>
   );
 }
 

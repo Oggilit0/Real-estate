d64830009c92c3f645e8624fe7bc3fbf8b6ac3aa
Oskar <Oskar.herdenberg@gmail.com>
2022-02-16 15:39:34 +0100

    Added structural code to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import LeftColumn from "./components/LeftColumn";
+import RightColumn from "./components/RightColumn";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
+      <div className="container">
+        <Routes>
+          <Main />
+          <RightColumn />
+          <LeftColumn />
+        </Routes>
+      </div>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 

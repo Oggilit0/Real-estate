2d46afd3623b56d6b09ed162bcef576124d723a3
Marcus Brederfält <marcus.brederfalt@gmail.com>
2022-02-21 15:49:15 +0100

    Added logo to TopImage

--- src/components/TopImage.js
+++ src/components/TopImage.js
@@ -1,6 +1,9 @@
+import Logo from "./Pictures/logga.png";
+
 const TopImage = () => {
   return (
     <div className="topImage" id="tempDesign">
+      <img src={Logo} width="100px" />
       <h2>Image</h2>
     </div>
   );

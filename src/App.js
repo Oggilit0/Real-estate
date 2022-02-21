ae13efd51178a1a44da973786db5012d17afa12e
Marcus Brederfält <marcus.brederfalt@gmail.com>
2022-02-21 10:07:13 +0100

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import LeftColumn from "./components/LeftColumn";
 import RightColumn from "./components/RightColumn";
 import Main from "./components/Main";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="container">
-      <Header />
-      <Main />
-      <RightColumn />
-      <LeftColumn />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/" element={<About />} />
+        </Routes>
+
+        <RightColumn />
+        <LeftColumn />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 

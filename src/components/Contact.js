d05cb58d2b2d9f5fb2b6800c745d12cea7bdb0a5
Mathilda <frknilsson@hotmail.com>
2022-02-21 22:08:38 +0100

    Added content to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,28 @@ import "../stylesheets/Contact.css"
 
 const Contact = () => {
   return (
-    <div className="contact">
-    <h2>Contact Us</h2>
+    <div className= "contact">
+      <hr className = "line"></hr>
+    <h2 className= "title">Contact Us</h2>
+      <hr className = "line"></hr>
+
+      <div className ="contactForm">
+      <h2>Contact Form</h2>
+      <form className ="form">
+        <div className= "form-controll">
+        <input type ="text" id ="firstname" placeholder ="Name"/>
+        </div>
+        <div className= "form-controll">
+        <input type="text" id ="email" placeholder="Email"/>
+        </div>
+        <div className= "form-controll">
+        <input type="text" id ="message" placeholder="Message"/>
+        </div>
+      </form>
+    
+    </div>
+
+    
     </div>
   );
   };

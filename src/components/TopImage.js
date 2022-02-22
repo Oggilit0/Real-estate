c060eb909a93c0347338b3fde920594c80f3d6d4
Mathilda <frknilsson@hotmail.com>
2022-02-22 10:52:46 +0100

    added topImage

--- src/components/TopImage.js
+++ src/components/TopImage.js
@@ -1,10 +1,12 @@
 import Logo from "./Pictures/logga.png";
 import "../stylesheets/TopImage.css"
+import BlogImage from "./Pictures/blogImage.jpg";
 
 const TopImage = () => {
   return (
     <div className="topImage" id="tempDesign">
       <img src={Logo} width="100px" />
+      <img id="blogImage" src={BlogImage}/>
       <h2>Image</h2>
     </div>
   );

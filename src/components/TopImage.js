5bb74572293b16e9240dd8ff580a2d72956fb3c9
Mathilda <frknilsson@hotmail.com>
2022-02-22 11:31:46 +0100

    Added topImage

--- src/components/TopImage.js
+++ src/components/TopImage.js
@@ -1,13 +1,12 @@
 import Logo from "./Pictures/logga.png";
 import "../stylesheets/TopImage.css"
-import BlogImage from "./Pictures/blogImage.jpg";
+import "../stylesheets/BlogPost.css";
 
 const TopImage = () => {
   return (
     <div className="topImage" id="tempDesign">
-
+      <div id="blogImage"></div>
       <div id="banner"></div>
-
     </div>
   );
 };

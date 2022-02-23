b18dd037d8eedc765c0610dec651a197d260882a
Oskar <Oskar.herdenberg@gmail.com>
2022-02-23 16:49:17 +0100

    Updated landing page with more content

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,16 +2,15 @@ import bedroom from "./Pictures/StartPage/bedroom-1.jpg";
 import diningroom from "./Pictures/StartPage/dining-room-1.jpg";
 import entrance from "./Pictures/StartPage/entrance-1.jpg";
 import livingroom from "./Pictures/StartPage/living-room-1.jpg";
+import discover from "./Pictures/family-photo-moving.jpg";
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
       <h2>Popular homes</h2>
-      <article>
-      </article>
+      <article></article>
       <h2>From dream to reality</h2>
-      <article>
-        
+      <article id="img-collage">
         <div id="img-container">
           <img id="small-img1" src={bedroom} />
           <p>
@@ -35,8 +34,21 @@ const LandingPage = () => {
         <img id="medium-img" src={livingroom} />
         <img id="broad-img" src={diningroom} />
       </article>
+      <h2>Discover how much your home is worth</h2>
       <article>
-        <h2>Discover how much your home is worth</h2>
+        <div id="discover">
+          <img id="discover-img" src={discover} />
+          <div id="discover-text">
+            <h1>Try out our free home value estimator!</h1>
+            <p>
+              Discover the current market value of your home with a free,
+              instant property valuation report from home evaluator. There's no
+              hidden fees or charges - we'll collate all the latest data and
+              email you your report within just a few minutes.
+            </p>
+            <button>Try it out now!</button>
+          </div>
+        </div>
       </article>
       <article>
         <h2>Articles</h2>

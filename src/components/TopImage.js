e370a41e1af15c0daf88054e48251682fa456c40
Oskar <Oskar.herdenberg@gmail.com>
2022-02-23 10:28:39 +0100

    Added and implemented function for listing objects

--- src/components/TopImage.js
+++ src/components/TopImage.js
@@ -1,10 +1,12 @@
 import SearchBar from "./SearchBar";
+import ListViewings from "./ListViewings.js";
 
 const TopImage = () => {
   return (
     <div className="topImage" id="tempDesign">
       <div id="banner">
         <SearchBar />
+        <ListViewings name="Summerview" />
       </div>
     </div>
   );

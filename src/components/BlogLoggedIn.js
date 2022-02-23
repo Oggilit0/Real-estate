e4b10e82fcc84e50974f7188541ee7ca4315d02e
Marcus Brederfält <marcus.brederfalt@gmail.com>
2022-02-23 19:04:38 +0100

    Added two new files for Blog inlog

---
+++ src/components/BlogLoggedIn.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import LoginForm from "./LoginForm";
+
+const BlogLoggedIn = () => {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123",
+  };
+
+  const marcusUser = {
+    email: "marcus@brederfalt.com",
+    password: "guy",
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+
+    if (
+      (details.email == adminUser.email &&
+        details.password == adminUser.password) ||
+      (details.email == marcusUser.email &&
+        details.password == marcusUser.password)
+    ) {
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
+      setError("Details does not match");
+    }
+  };
+
+  const Logout = () => {
+    console.log("Logout");
+    setUser({ name: "", email: "" });
+  };
+
+  return (
+    <div className="App">
+      {user.email != "" ? (
+        <div>
+          <h2>
+            Welcome, <span>{user.name}</span>
+          </h2>
+          <h4>
+            Make a post.
+            <form></form>
+          </h4>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
+    </div>
+  );
+};
+
+export default BlogLoggedIn;

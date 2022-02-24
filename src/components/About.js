0de4ff46ec707f938b46a004a9d8fdaeb809423d
Marcus Brederfält <marcus.brederfalt@gmail.com>
2022-02-24 11:16:41 +0100

    updated about page, added picture

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,31 @@
+import aboutPhoto from "./Pictures/about.jpg";
+
 const About = () => {
   return (
-    <>
-      <h2>Yes we are real, Real Estate </h2>
-      <p />
-      Real Estate was founded in 1996 when co workers Lars Björnsson reached out
-      to Guy Adams, the two of them met while working at a large real estate
-      firm. but both had get tired of the stress in the workplace and that it
-      was all about sales.
-      <p /> They wanted to shift the focus to the most important aspect in Real
-      Estate, the client, and that is you!
-      <p />
-      oday Lars and Guy works with two other agents, our door is always open for
-      you, why don't come in for a chat over a cup of coffee? Besides offering
-      good objects to match your dreams we have other services to help you on
-      your quest, you can read more about that on our service page.
-    </>
+    <div className="contact">
+      <hr className="line"></hr>
+      <h2 className="title">Yes we are real, Real Estate </h2>
+      <hr className="line"></hr>
+
+      <div className="aboutRL">
+        Real Estate was founded in 1996 when co workers Louise Björnsson reached
+        out to Guy Adams, the two of them met while working at a large real
+        estate firm. but both had get tired of the stress in the workplace and
+        that it was all about sales. They wanted to shift the focus to the most
+        important aspect in real estate, the client, and that is you!
+        <p />
+        Louise and Guy works with three other agents, our door is always open
+        for you, why don't come in for a chat over a cup of coffee? Besides
+        offering good objects to match your dreams we have other services to
+        help you on your quest, you can read more about that on our service
+        page.
+      </div>
+      <div className="aboutHiring">
+        We are always looking for new talents, please contact us if you are
+        interested.
+        <img id="aboutPhoto" src={aboutPhoto} />
+      </div>
+    </div>
   );
 };
 

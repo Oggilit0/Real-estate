f5349e2a85c806870e34bb39979bd98c790e2424
Marcus Brederfält <marcus.brederfalt@gmail.com>
2022-02-24 08:37:24 +0100

    Started to update the page

--- src/components/About.js
+++ src/components/About.js
@@ -1,45 +1,15 @@
 const About = () => {
   return (
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-      tincidunt elit diam, et gravida diam fringilla ac. Donec eget nibh id
-      nulla feugiat interdum. Ut id mauris vel quam cursus laoreet. Donec
-      consectetur blandit risus ut commodo. Orci varius natoque penatibus et
-      magnis dis parturient montes, nascetur ridiculus mus. Pellentesque
-      habitant morbi tristique senectus et netus et malesuada fames ac turpis
-      egestas. Vestibulum id auctor quam. Sed lobortis efficitur dui ac dapibus.
-      Aenean a lacinia sapien. Fusce laoreet felis in diam porttitor mollis. Sed
-      risus justo, egestas ut vulputate id, consectetur sit amet lacus. Cras
-      dignissim eu risus non tincidunt. Sed et augue quis eros aliquam tincidunt
-      ut id ipsum. Nunc ligula augue, hendrerit ac magna ac, auctor tristique
-      velit. Suspendisse potenti. Curabitur volutpat elit in congue laoreet.
-      Duis ac efficitur tortor. Vestibulum a tincidunt nulla. Vivamus sed leo
-      gravida sapien ultricies rutrum quis sed diam. Fusce tempus, magna at
-      bibendum dignissim, mi velit iaculis justo, non sollicitudin velit nisl
-      vitae nunc. Sed id tellus sit amet magna tincidunt rutrum ac et enim. Sed
-      laoreet elit vitae magna vestibulum dictum. Duis egestas gravida dui, et
-      laoreet velit laoreet at. Duis fermentum, massa eleifend malesuada
-      lacinia, sapien quam euismod libero, vitae fermentum eros ante vel velit.
-      Quisque sit amet auctor est. Vestibulum lobortis neque vel nisi blandit,
-      in dictum elit luctus. Integer laoreet nulla sapien, et auctor sapien
-      hendrerit nec. Phasellus sit amet tincidunt lacus, eget euismod sapien.
-      Nunc quis augue ultricies, egestas lacus nec, varius velit. Sed vehicula
-      magna iaculis erat sodales varius. Duis ultricies efficitur eros vel
-      suscipit. Donec quis luctus diam. Suspendisse convallis tincidunt
-      vulputate. Sed posuere purus nibh. In malesuada nulla quis nibh tempor
-      mattis. Nunc auctor elementum suscipit. Cras in nibh erat. Sed rhoncus sem
-      sed elit feugiat maximus. Aenean euismod, lacus vel placerat viverra, mi
-      ex rutrum lacus, sed efficitur mauris nunc ac est. Vivamus id ex vitae
-      eros rhoncus pharetra ut malesuada velit. Sed eros mi, rutrum id porta
-      vel, gravida a urna. Cras dui turpis, pretium quis aliquam at, ornare in
-      orci. Ut eleifend condimentum neque condimentum blandit. Maecenas
-      consectetur lacinia lorem, nec finibus purus rutrum et. Praesent semper
-      nisl eu eleifend efficitur. Curabitur egestas placerat accumsan.
-      Pellentesque semper sit amet augue vitae egestas. Integer ut sem rutrum,
-      pretium dui in, ultrices nisi. Vivamus nec arcu commodo mi posuere maximus
-      ut ut ante. Integer faucibus volutpat elementum. Donec facilisis augue
-      metus, feugiat ornare nisi faucibus et.
-    </p>
+    <>
+      <h2>Yes we are real, Real Estate </h2>
+      <p />
+      Real Estate was founded in 1996 when co workers Lars Björnsson reached out
+      to Guy Adams, the two of them met while working at a large real estate
+      firm. but both had get tired of the stress in the workplace and that it
+      was all about sales.
+      <p /> They wanted to shift the focus to the most important aspect in Real
+      Estate, the client, and that is you!
+    </>
   );
 };
 

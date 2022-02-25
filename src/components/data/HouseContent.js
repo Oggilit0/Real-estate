4a4b461db51fb38221095a8f2f7e5cd8e7a44cd4
Marcus Brederfält <marcus.brederfalt@gmail.com>
2022-02-25 14:53:47 +0100

    Added function to list free houses on buy page

---
+++ src/components/data/HouseContent.js
@@ -0,0 +1,37 @@
+import img1 from "../Pictures/real-estate-agent.jpg";
+
+const HouseContent = [
+  {
+    id: 1,
+    city: "Gothenburg",
+    address: "Address: Vasaplatsen 5",
+    price: "Asking price: 2 kr",
+    sqMeters: "Square meters: 30",
+    amountRooms: "Amount of rooms: 2",
+    type: "Type of living: Apartment",
+    description: "Now you have the chance to aquire this apartment",
+    image: img1,
+  },
+  {
+    id: 2,
+    city: "Kinna",
+    address: "Address: Main Street 5",
+    price: "Asking price: 300 kr",
+    sqMeters: "Square meters: 80",
+    amountRooms: "Amount of rooms: 5",
+    type: "Type of living: Apartment",
+    description: "Now you have the chance to aquire this apartment",
+  },
+  {
+    id: 3,
+    city: "Boden",
+    address: "Address: Kungsgatan 3",
+    price: "Asking price: 40 kr",
+    sqMeters: "Square meters: 70",
+    amountRooms: "Amount of rooms: 4",
+    type: "Type of living: Apartment",
+    description: "Now you have the chance to aquire this apartment",
+  },
+];
+
+export default HouseContent;

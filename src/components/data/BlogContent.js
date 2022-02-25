19bc6066658b3e9ac573b022ed8602feb5e03547
Mathilda <frknilsson@hotmail.com>
2022-02-25 09:36:22 +0100

    Added photos and text to blog

--- src/components/data/BlogContent.js
+++ src/components/data/BlogContent.js
@@ -1,34 +1,39 @@
-
+import img1 from "../Pictures/real-estate-agent.jpg";
+import img2 from "../Pictures/new-york.jpg";
+import img3 from "../Pictures/house-for-sale.jpg";
 
 const BlogContent = [
 {
   id:1,
-  title:"Mathilda är bra", 
-  date:"2022-02-24",
-  author:"Mathilda Nilsson",
-  text:"Hallååå hallååå", 
-  previewText:"Hall...",
+  title:"ONCE UPON A REAL ESTATE AGENT", 
+  date:"2021-02-24",
+  time:"15:36",
+  author:"Laureen	Goodere",
+  image:img1,
+  text:"My real estate journey have been interesting and exciting.", 
+  previewText:"My real estate journey have been interesting and exciting.",
  
 },
 {
   id:2,
-  title:"Mathilda är bra", 
-  date:"2022-02-24",
-  author:"Mathilda Nilsson",
-  text:"",/*"My real estate journey have been interesting and exciting. I started
-  here 4 years ago as a secretary for one of the biggest real estate
-  agents in the city. That's not something I would like to do for the rest
-  of my life, so after 3 years I was offered a chance to be an agent
-  myself. I had already so many great experiences and have seen so much
-  that I thought this is gonna be a whole new ball game for me.."*/
+  title:"UPCOMING LISTINGS IN NEW YORK", 
+  date:"2021-05-02",
+  time:"08:29",
+  author:"Stacey Wilson",
+  image: img2,
+  text:"",
+  previewText:""
   
 },
 {
   id:3,
-  title:"Mathilda är bra", 
-  date:"2022-02-24",
-  author:"Mathilda Nilsson",
-  text:"Hallååå hallååå", 
+  title:"5 TIPS WHEN SELLING YOUR HOUSE", 
+  date:"2021-05-10",
+  time:"10:07",
+  author:"Adam Yarkoni",
+  image: img3,
+  text:"",
+  previewText:""
   
 }
 ];

9d2df8d1a8ab6e7e5970600c3be6e6f593db14a4
Marcus Brederfält <marcus.brederfalt@gmail.com>
2022-02-26 15:50:36 +0100

    Added more on housecontent

--- src/components/data/HouseContent.js
+++ src/components/data/HouseContent.js
@@ -2,6 +2,9 @@ import img1 from "../Pictures/House/House1/House1-1.jpg";
 import img2 from "../Pictures/House/House2/House2-1.jpg";
 import img3 from "../Pictures/House/House3/House3-1.jpg";
 import img4 from "../Pictures/House/House4/House4-2.jpg";
+import img5 from "../Pictures/House/House5/House5-1.jpg";
+import img6 from "../Pictures/House/House6/House6-1.jpg";
+import img7 from "../Pictures/House/House7/House7-1.jpg";
 
 const HouseContent = [
   {
@@ -50,14 +53,37 @@ const HouseContent = [
   },
   {
     id: 5,
-    city: "Valdemarsvik",
-    address: "Udden 5",
-    price: "Asking price: 5000000kr",
-    sqMeters: "Square meters: 200",
-    amountRooms: "Amount of rooms: 3",
+    city: "Bettna",
+    address: "Lidén 3",
+    price: "Asking price: 1000000kr",
+    sqMeters: "Square meters: 100",
+    amountRooms: "Amount of rooms: 2",
     type: "Type of living: House",
+    description: "A medow on the country side..",
+    image: img5,
+  },
+
+  {
+    id: 6,
+    city: "Lund",
+    address: "Prinsessgatan 1",
+    price: "Asking price: 8000000kr",
+    sqMeters: "Square meters: 66",
+    amountRooms: "Amount of rooms: 2",
+    type: "Type of living: House",
+    description: "",
+    image: img6,
+  },
+  {
+    id: 7,
+    city: "Stockholm",
+    address: "Strandvägen 5",
+    price: "Asking price: 50000000kr",
+    sqMeters: "Square meters: 200",
+    amountRooms: "Amount of rooms: 4",
+    type: "Type of living: Apartment",
     description: "A place to build a family",
-    image: img4,
+    image: img7,
   },
 ];
 

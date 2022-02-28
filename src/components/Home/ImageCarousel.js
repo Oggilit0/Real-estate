b93511367a8c147ccb46e8f834af5d4dd8dfc952
Mathilda <frknilsson@hotmail.com>
2022-02-28 21:45:07 +0100

    added pictures to image carousel

--- src/components/Home/ImageCarousel.js
+++ src/components/Home/ImageCarousel.js
@@ -1,5 +1,13 @@
 import Carousel from "react-elastic-carousel";
 import discover from "../Pictures/pillow.jpg";
+import house2 from "../Pictures/House/House2/House2-3.jpg";
+import house3 from "../Pictures/House/House3/House3-4.jpg";
+import house4 from "../Pictures/House/House4/House4-5.jpg";
+import house5 from "../Pictures/House/House5/House5-1.jpg";
+import house6 from "../Pictures/House/House6/House6-1.jpg";
+import house7 from "../Pictures/House/House7/House7-1.jpg";
+import house8 from "../Pictures/House/Extras/House8-1.jpg";
+import house9 from "../Pictures/House/Extras/House9-1.jpg";
 import Item from "./Item";
 import { Link } from "react-router-dom";
 
@@ -22,42 +30,42 @@ const ImageCarousel = () => {
         >
           <Item>
             <Link to="/about">
-              <img id="carousel-img" src={discover} />
+              <img id="carousel-img" src={house3} />
             </Link>
           </Item>
           <Item>
             <Link to="/about">
-            <img id="carousel-img" src={discover} />
+            <img id="carousel-img" src={house2} />
             </Link>
           </Item>
           <Item>
           <Link to="/about">
-            <img id="carousel-img" src={discover} />
+            <img id="carousel-img" src={house4} />
             </Link>
           </Item>
           <Item>
           <Link to="/about">
-            <img id="carousel-img" src={discover} />
+            <img id="carousel-img" src={house6} />
             </Link>
           </Item>
           <Item>
           <Link to="/about">
-            <img id="carousel-img" src={discover} />
+            <img id="carousel-img" src={house5} />
             </Link>
           </Item>
           <Item>
           <Link to="/about">
-            <img id="carousel-img" src={discover} />
+            <img id="carousel-img" src={house7} />
             </Link>
           </Item>
           <Item>
           <Link to="/about">
-            <img id="carousel-img" src={discover} />
+            <img id="carousel-img" src={house8} />
             </Link>
           </Item>
           <Item>
           <Link to="/about">
-            <img id="carousel-img" src={discover} />
+            <img id="carousel-img" src={house9} />
             </Link>
           </Item>
         </Carousel>

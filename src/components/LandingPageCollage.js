fd5822c5018409cd08563531464fb9ffaff92b30
Oskar <Oskar.herdenberg@gmail.com>
2022-02-28 09:10:35 +0100

    Added new components with code from landing page

---
+++ src/components/LandingPageCollage.js
@@ -0,0 +1,38 @@
+import diningroom from "./Pictures/StartPage/dining-room-1.jpg";
+import entrance from "./Pictures/StartPage/entrance-1.jpg";
+import livingroom from "./Pictures/StartPage/living-room-1.jpg";
+import bedroom from "./Pictures/StartPage/bedroom-1.jpg";
+
+const LandingPageCollage = () => {
+  return (
+    <article className="landing-article" id="img-collage">
+      <section className="landing-section" id="img-container">
+        <img id="small-img1" src={bedroom} />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel
+          sodales magna. Duis tristique a augue ac hendrerit. Maecenas mollis,
+          sapien quis sollicitudin convallis, ex diam posuere urna, in euismod
+          magna lorem ut velit.
+        </p>
+      </section>
+
+      <section className="landing-section" id="img-container">
+        <img id="small-img2" src={entrance} />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel
+          sodales magna. Duis tristique a augue ac hendrerit. Maecenas mollis,
+          sapien quis sollicitudin convallis, ex diam posuere urna, in euismod
+          magna lorem ut velit.
+        </p>
+      </section>
+
+      <section className="landing-section" id="medium-img-container">
+        <img id="medium-img" src={livingroom} />
+      </section>
+      <section className="landing-section" id="broad-img-container">
+        <img id="broad-img" src={diningroom} />
+      </section>
+    </article>
+  );
+};
+export default LandingPageCollage;

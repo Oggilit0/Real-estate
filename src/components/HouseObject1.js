d56f1e26f4416aba694921df257c9c38ad0dee3b
marcus <marcus.brederfalt@gmail.com>
2022-02-28 16:41:29 +0100

    Added changes to Houseboject

--- src/components/HouseObject1.js
+++ src/components/HouseObject1.js
@@ -6,7 +6,7 @@ import pic5 from "./Pictures/House/House1/House1-5.jpg";
 
 const HouseObject1 = () => {
   return (
-    <div className="houseobject">
+    <div className="houseObjectContainer">
       <div className="factsTop">
         <hr className="line"></hr>
         <h2 className="title">Facts</h2>
@@ -14,7 +14,7 @@ const HouseObject1 = () => {
       </div>
 
       <div className="adressPrice">
-        <div className="adress">Vasaplatsen 5</div>
+        <div className="adress">Ödåkra 5</div>
         <div className="price">2000009kr</div>
       </div>
 
@@ -25,15 +25,20 @@ const HouseObject1 = () => {
       <div className="display">
         <img src={pic1} className="image" />
         <img src={pic2} className="image" />
-      </div>
-
-      <div className="display2">
         <img src={pic3} className="image" />
         <img src={pic4} className="image" />
       </div>
 
-      <div className="display3">
-        <img src={pic5} className="image" />
+      <div className="description-bar">
+        <hr className="line"></hr>
+        <h4>
+          Welcome to Ödåkra 5! <p /> This house is just an hour drive from the
+          city centre of Gothenburg. It has two floors and 70 square meters of
+          space, the house has two bedrooms, and three toilets. A big kitchen
+          its a must when living in a house with this caliber. And just outside
+          is the luxury pool where you can relax after a hard days work. Welcome
+          to book a viewing!
+        </h4>
       </div>
     </div>
   );

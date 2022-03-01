9b7c95dd6f2b572ccb46ad2b1fd5a73a4a586748
Oskar <Oskar.herdenberg@gmail.com>
2022-03-01 15:32:23 +0100

    Added style for a responsive menu

--- src/components/Header/NavigationBar.js
+++ src/components/Header/NavigationBar.js
@@ -1,8 +1,15 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 
 const NavigationBar = () => {
   return (
     <nav id="navbar">
+      <input type="checkbox" id="check" />
+      <label for="check" class="checkbtn">
+      <FontAwesomeIcon icon={faBars}/>
+      </label>
       <ul>
         <li>
           <Link to="/">Home</Link>

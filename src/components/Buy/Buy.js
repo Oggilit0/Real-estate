a67c1f62f354e8e32d6d0a894fc2a7983b084528
Oskar <Oskar.herdenberg@gmail.com>
2022-03-02 15:30:16 +0100

    Added useEffect to topimage in every affected component

--- src/components/Buy/Buy.js
+++ src/components/Buy/Buy.js
@@ -3,12 +3,15 @@ import HouseContent from "../data/HouseContent";
 import HouseList from "./HouseList";
 import { useEffect } from "react";
 import Inspirational from "../ReusableComponents/Inspirational";
+import img from "../Pictures/bg-image.jpg";
 import '../../stylesheets/Buy.css';
 
 const Buy = () => {
   const [post, setPost] = useState(HouseContent);
   useEffect(() => {
+
     document.title = "Buy";
+    document.getElementById("banner").style.backgroundImage = `url(${img})`;
   }, []);
 
   return (

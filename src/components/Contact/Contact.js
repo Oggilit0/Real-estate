2f4d99f11f57ed68c705b0f06671cee3035ec7f7
Oskar <Oskar.herdenberg@gmail.com>
2022-03-03 16:22:37 +0100

    Fixed contacts responsive design

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,12 +8,11 @@ import laura from "../Pictures/contact/laura.jpg";
 import miles from "../Pictures/contact/miles.jpg";
 import { useEffect } from "react";
 import ReusableButton from "../ReusableComponents/ReusableButton";
-import '../../stylesheets/Contact.css';
+import "../../stylesheets/Contact.css";
 import Career from "../Contact/Career";
 import img from "../Pictures/bg-image.jpg";
 
 const Contact = () => {
-
   useEffect(() => {
     document.title = "Contact";
     window.scrollTo(0, 0);
@@ -22,9 +21,10 @@ const Contact = () => {
 
   return (
     <div className="contact">
-
       <div className="contactForm">
-        <h2 className = "title" id="contactForm">Contact Form</h2>
+        <h2 className="title" id="contactForm">
+          Contact Form
+        </h2>
         <form className="form">
           <div className="form-controll">
             <input type="text" id="firstname" placeholder="Name" />
@@ -38,66 +38,79 @@ const Contact = () => {
           <ReusableButton title="Send"></ReusableButton>
         </form>
       </div>
-      
+
       <div id="contactTopTitle">
-      <hr className="line"></hr>
-      <h2 className="title">Contact Us</h2>
-      <hr className="line"></hr>
+        <hr className="line"></hr>
+        <h2 className="title">Contact Us</h2>
+        <hr className="line"></hr>
       </div>
 
-    
       <div id="articles-container">
         <div id="articles-img-container">
           <img id="contact-img" src={louise}></img>
-          <p>Louise Yarkoni</p>
-          <p>CEO and founder</p>
+          <div id="contact-info">
+            <p>Louise Yarkoni</p>
+            <p>CEO and founder</p>
+          </div>
         </div>
         <div id="articles-img-container">
           <img id="contact-img" src={guy}></img>
-          <p>Guy Adams</p>
-          <p>Founder and KAM</p>
+          <div id="contact-info">
+            <p>Guy Adams</p>
+            <p>Founder and KAM</p>
+          </div>
         </div>
         <div id="articles-img-container">
           <img id="contact-img" src={laura}></img>
-          <p>Laura Carter</p>
-          <p>HR manager</p>
+          <div id="contact-info">
+            <p>Laura Carter</p>
+            <p>HR manager</p>
+          </div>
         </div>
         <div id="articles-img-container">
           <img id="contact-img" src={caroll}></img>
-          <p>Caroll Adams</p>
-          <p> Sales manager</p>
+          <div id="contact-info">
+            <p>Caroll Adams</p>
+            <p> Sales manager</p>
+          </div>
         </div>
         <div id="articles-img-container">
           <img id="contact-img" src={miles}></img>
-          <p>Miles Ray</p>
-          <p>IT Engineer</p>
+          <div id="contact-info">
+            <p>Miles Ray</p>
+            <p>IT Engineer</p>
+          </div>
         </div>
         <div id="articles-img-container">
           <img id="contact-img" src={adam}></img>
-          <p>Adam Yarkoni</p>
-          <p>Sales executive</p>
+          <div id="contact-info">
+            <p>Adam Yarkoni</p>
+            <p>Sales executive</p>
+          </div>
         </div>
         <div id="articles-img-container">
           <img id="contact-img" src={margret}></img>
-          <p>Margret Silverstein</p>
-          <p>Sales executive</p>
+          <div id="contact-info">
+            <p>Margret Silverstein</p>
+            <p>Sales executive</p>
+          </div>
         </div>
         <div id="articles-img-container">
           <img id="contact-img" src={annika}></img>
-          <p>Annika Devald</p>
-          <p>Reception</p>
+          <div id="contact-info">
+            <p>Annika Devald</p>
+            <p>Reception</p>
+          </div>
         </div>
       </div>
 
       <div id="careerTitle">
-      <hr className="line"></hr>
-      <h2 className="title">Career</h2>
-      <hr className="line"></hr>
+        <hr className="line"></hr>
+        <h2 className="title">Career</h2>
+        <hr className="line"></hr>
       </div>
 
-      <Career/>
-
-      
+      <Career />
     </div>
   );
 };
